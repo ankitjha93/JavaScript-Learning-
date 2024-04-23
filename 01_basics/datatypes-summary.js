4f43c05c9a0653a579fe2fd0ba28e9b327236ee6
Ankit Jha <95637122+ankitjha31@users.noreply.github.com>
2024-04-23 10:11:09 +0000

    datatypes and comparison

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 345667786798987n;
+
+// const heros = ["Shaktiman", "Doga", "Naagraj"];
+
+// let myObj = {
+//     name: "hitesh",
+//     age:23,
+// }
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof bigNumber)
