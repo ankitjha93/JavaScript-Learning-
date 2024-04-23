67d16edbb95889cf5db9d007ecbc0cb4b69b6d4e
Ankit Jha <95637122+ankitjha31@users.noreply.github.com>
2024-04-23 06:43:34 +0000

    convertion of operations

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,16 @@
+let score = "ankit"
+// console.log( typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "ankit";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber);
