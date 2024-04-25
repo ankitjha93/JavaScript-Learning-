851b812e8dbf289076c77e56cc12cf908ac4ef87
Ankit Jha <95637122+ankitjha31@users.noreply.github.com>
2024-04-25 06:55:39 +0000

    math and nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 100;
+// console.log(score)
+
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 123.5559;
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 100000000;
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+//************************* Maths ***************************/
+
+// console.log(Math.abs(-5))
+// console.log(Math.round(5.77))
+// console.log(Math.ceil(9.99));
+// console.log(Math.floor(4.55))
+
+// console.log(Math.min(2,31,343,545,6,1));
+// console.log(Math.max(34,23,123,54,45));
+
+// console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
