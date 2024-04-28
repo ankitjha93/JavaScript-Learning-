49ad2ffff349a3b7cced48e285d67f39e2601591
Ankit Jha <95637122+ankitjha31@users.noreply.github.com>
2024-04-28 15:37:45 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,54 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element == 5 ){
+        // console.log("5 is the best number");
+    }
+    // console.log(element);
+    
+}
+
+for (let i = 1; i <= 10; i++) {
+    // const element = array[i];
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // const element = array[j];
+        // console.log(`Inner loop value ${j} and Inner loop ${j}`);
+        // console.log(i + '*' + j + ' = ' + i*j);
+        
+    }
+    
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+
+
+// console.log(myArray.length)
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     // const element = array[index];
+//     if(index == 5){
+//         console.log(`Detected 5`);
+//         break;
+//     }
+//     console.log(`Value is : ${index}`);
+    
+// }
+for (let index = 1; index <= 20; index++) {
+    // const element = array[index];
+    if(index == 5){
+        console.log(`Detected 5`);
+        continue;
+    }
+    console.log(`Value is : ${index}`);
+    
+}
