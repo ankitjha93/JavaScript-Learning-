2c9dda2563595cfa88aceee5c4bbc96fee749f2d
Ankit Jha <95637122+ankitjha31@users.noreply.github.com>
2024-04-28 14:41:34 +0000

    control flow in one short

---
+++ 04_control_flow/one.js
@@ -0,0 +1,65 @@
+// if(true){
+
+// }
+
+// if(false){
+
+// }
+
+// const isUserLoggedIn = true;
+
+// if(isUserLoggedIn){
+    
+// }
+
+// if(2 === "2"){
+//     console.log("executed");
+// }
+
+// const temperature = 45;
+
+// if(temperature === 50){
+//     console.log("Temperature is less than 50")
+// }
+// else{
+// console.log("Temperatur is greater than 50");
+// }
+
+// const score = 200;
+
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`User Power : ${power}`);
+
+// }
+
+
+// console.log(`User Power : ${power}`);
+
+// const balance = 1000;
+
+// if(balance > 500) console.log("test"),
+// console.log("test 2")
+// if(balance < 500){
+//     console.log("less than 500");
+// }else if(balance < 750){
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const isUserLoggedIn = true;
+
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+// if(isUserLoggedIn && debitCard){
+//     console.log("Allow to buy course");
+// }
+
+if(loggedInFromEmail || loggedInFromEmail){
+    console.log("user logged in");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March";
+
+switch(month){
+    case "Jan":
+        console.log("Jan");
+        break;
+    case "Feb": 
+       console.log("Feb");
+       break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    default:
+        console.log("Default case matched");
+        break;
+}
